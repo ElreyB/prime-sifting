d7100701ce205304fb0f308c5d9193107435f2f6
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-07 11:36:15 -0700

    allattrabutes are readable

--- lib/prime_sifting.rb
+++ lib/prime_sifting.rb
@@ -1,3 +1,18 @@
 class PrimeSifting
+  attr_reader :count_to, :prime_numbers, :list_of_numbers
   
+  def initialize(count_to)
+    @count_to = count_to
+    @list_of_numbers = numbers
+    @prime_numbers = []
+  end
+
+private
+  def numbers
+    numbers = []
+    for number in 2..@count_to do
+      numbers << number
+    end
+    numbers
+  end
 end
--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -2,13 +2,18 @@ require 'rspec'
 require 'prime_sifting'
 
 describe 'PrimeSifting' do
-  let(:number) { PrimeSifting.new(30) }
+  let(:number) { PrimeSifting.new(20) }
+  let(:two_to_numbers) { [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}
 
   it 'will have a readable count to number' do
-    expect(number.count_to).to eq 30
+    expect(number.count_to).to eq 20
   end
 
   it 'will have a readable prime number list' do
     expect(number.prime_numbers).to eq []
+  end
+
+  it 'will have a readable list of number from 2 to number given' do
+    expect(number.list_of_numbers).to eq two_to_numbers
   end
 end
